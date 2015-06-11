2b0d0fa8e26efeba4bc43216ec1454c3ea31f967
Colin <366760348@qq.com>
2015-06-11 13:08:07 +0800

    函数

--- Ruby/p4.py
+++ Ruby/p4.py
@@ -39,3 +39,78 @@ def printme(str):
 printme("调用用户自定义函数")
 printme("再次调用同一函数")
 
+#可写函数说明
+def changename(mylist):
+	"修改传入的列表"
+	mylist.append([1,2,3,4])
+	print "函数内取值", mylist
+	return
+    
+
+#调用changename函数
+mylist = [10,20,30];
+changename(mylist);
+print "函数外取值：", mylist
+
+#参数
+def printinfo(name,age):
+	print "Name :",name
+	print "Age : ", age 
+	return
+
+#调用
+printinfo("YX",17)
+
+#不定长参数
+def printinfot(arg1,*vartuple):
+	"打印传入的任何参数"
+	print "输出:"
+	print arg1
+	for var in vartuple:
+		print var
+	return;
+
+#调用函数
+printinfot(70,80,90)
+
+#return
+def sum(a1,a2):
+	#返回两参数的和
+	total = a1 + a2 
+	print "Inside the function: ", total 
+	return total;
+
+
+#调用
+sum(10,20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
