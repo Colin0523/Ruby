7feff613015171af4033225713f776bc6a451dc3
Colin <366760348@qq.com>
2015-07-08 19:12:48 +0800

    列表方法

--- Ruby/pythoncontrol.py
+++ Ruby/pythoncontrol.py
@@ -1,3 +1,4 @@
+# -*-coding: utf-8 -*-
 #for
 webster = {
 	"Aardvark" : "A star of a popular children's cartoon show.",
@@ -15,3 +16,17 @@ a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 for num in a:
     if num % 2 == 0:
         print num
+
+
+#list functions
+def fizz_count(x):
+    count = 0
+    for item in x:
+        if item == 'fizz':
+            count += 1
+        
+    
+    print '次数是'+  str(count)
+  
+fizz = ["fizz","cat","fizz"]
+fizz_count(fizz)
