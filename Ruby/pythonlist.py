cc6575945fbe6a2ea2748f351734d6c4d80ee6d0
Colin <366760348@qq.com>
2015-07-14 18:27:08 +0800

    list

--- Ruby/pythonlist.py
+++ Ruby/pythonlist.py
@@ -171,6 +171,42 @@ inventory['backpack'].sort()
 inventory['backpack'].remove('dagger')
 inventory['gold'] = 550
 
+########################list tips
+#n.pop(index) will remove the item at index from the list and return it to you:n = [1, 3, 5]
+#n.pop(1)
+# Returns 3 (the item at index 1)
+#print n
+# prints [1, 5]
+
+#n.remove(item) will remove the actual item if it finds it:
+#n.remove(1)
+# Removes 1 from the list,
+# NOT the item at index 1
+#print n
+# prints [3, 5]
+#del(n[1]) is like .pop in that it will remove the item at the given index, but it won't return it:
+#del(n[1])
+# Doesn't return anything
+#print n
+# prints [1, 5]
+###########################
+
+
+n = [3, 5, 7]
+def double_list(x):
+    for i in range(0, len(x)):
+        x[i] = x[i] * 2
+    return x
+# Don't forget to return your new list!
+
+print double_list(n)
+
+
+
+
+
+
+
 
 
 
